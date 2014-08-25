f5e8114d907962919263bcb79eda89a36be26032
Sebastian Geiger <sebastian.geiger@gmail.com>
2014-08-25 19:38:02 -0400

    << works

--- lib/list.rb
+++ lib/list.rb
@@ -1,19 +1,18 @@
 class List
-  attr_reader :first
-
   def self::[](*elements)
     new(elements)
   end
 
   def initialize(elements)
-    @first = Element.new(elements.shift, :first_element, nil)
+    @first = Element.new(elements.shift, NOP.new, nil)
     current = @first
     until elements.empty?
       previous = current
       current = Element.new(elements.shift, previous, nil)
       previous.next = current
     end
-    current.next = :last_element
+    @last = current
+    current.next = NOP.new
   end
 
   def [](i)
@@ -29,17 +28,41 @@ class List
     return_value = @first.payload
     new_first = get_element(1)
     @first.next = nil
-    new_first.previous = :first_element
+    new_first.previous = NOP.new
     @first = new_first
     return_value
   end
 
   def unshift(element)
-    element = Element.new(element, :first_element, @first)
+    element = Element.new(element, NOP.new, @first)
     @first.previous = element
     @first = element
   end
 
+  def <<(element)
+    element = Element.new(element, self.last, NOP.new)
+    @last.next = element
+    @first = element if @last.nop?
+    @last = element
+    self
+  end
+
+  def size
+    length = 0
+    current = self.first
+    until current.nop?
+      current = current.next
+      length += 1
+    end
+    length
+  end
+
+  def first
+    @first ||= NOP.new
+  end
+  def last
+    @last ||= NOP.new
+  end
   private
   def get_element(i)
     current = @first
@@ -49,7 +72,23 @@ class List
     end
     current
   end
-  class Element < Struct.new(:payload, :previous, :next)
 
+  class Element < Struct.new(:payload, :previous, :next)
+    def nop?
+      false
+    end
+  end
+
+  class NOP
+    def next=(element)
+    end
+    def previous=(element)
+    end
+    def ==(other)
+      true
+    end
+    def nop?
+      true
+    end
   end
 end
