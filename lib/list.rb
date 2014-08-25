7c13a0e2142594168203ed5ff7afbdca9300b5c3
Sebastian Geiger <sebastian.geiger@gmail.com>
2014-08-25 18:44:05 -0400

    ::[] works

--- lib/list.rb
+++ lib/list.rb
@@ -1,1 +1,29 @@
-# Implement List here
+class List
+  def self::[](*elements)
+    new(elements)
+  end
+
+  def initialize(elements)
+    @first = Element.new(elements.shift, :first_element, nil)
+    current = @first
+    until elements.empty?
+      p current
+      previous = current
+      current = Element.new(elements.shift, previous, nil)
+      previous.next = current
+    end
+  end
+
+  def [](i)
+    current = @first
+    until i <= 0
+      i -= 1
+      current = current.next
+    end
+    current.payload
+  end
+
+  class Element < Struct.new(:payload, :next, :previous)
+
+  end
+end
