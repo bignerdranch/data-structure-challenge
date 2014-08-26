d9ee329c042631440e8ab8cc7ae70c12409523c4
Shelby Switzer <shelby@voterheads.com>
2014-08-25 23:24:09 -0400

    success!

--- lib/list.rb
+++ lib/list.rb
@@ -1,11 +1,15 @@
 # Implement List here
 
 class List
-  attr_reader :size
+  attr_accessor :size
 
   # List[]
   def self.[](*items)
     new(items)
+  end
+
+  def self.allocate
+    super().tap { |obj| obj.size = 0 }
   end
 
   def initialize(items)
@@ -13,33 +17,29 @@ class List
       instance_variable_set("@a#{i}", item)
     end
     @size = items.length.to_i
+    # @first = :@a0
   end
 
   # list = List.new
   # list[]
   # list[0]  @a0
   def [](num)
-    if num.to_s.start_with?("-")
-      place = @size + num
-      instance_variable_get("@a#{place}")
-    else
-      instance_variable_get("@a#{num}")
-    end
-
+    return nil if num > (@size - 1) || (num * -1) > @size
+    instance_variable_get("@a#{num % @size}")
   end
 
   def ==(list)
     list.instance_variables.all? do |var|
-      self.instance_variables.detect { |v| instance_variable_get(v) == list.instance_variable_get(var) }
+      instance_variables.detect { |v| instance_variable_get(v) == list.instance_variable_get(var) }
     end
   end
 
   def shift
     removed = remove_instance_variable("@a0")
-    @size = @size.nil? ? 0 : (@size - 1)
-    instance_variables.each do|var|
-      next if var == :size
-      num = var.to_s.scan(/\d/).join("")
+    @size -= 1
+    instance_variables.each do |var|
+      next if var == :@size
+      num = var.to_s.scan(/\d+/).join("")
       instance_variable_set("@a#{num.to_i - 1}", instance_variable_get(var))
     end
     removed
@@ -49,22 +49,24 @@ class List
     @size = @size.nil? ? 0 : (@size + 1)
     instance_variables.each do|var|
       next if var == :size
-      num = var.to_s.scan(/\d/).join("")
+      num = var.to_s.scan(/\d+/).join("")
       instance_variable_set("@a#{num.to_i + 1}", instance_variable_get(var))
     end
     instance_variable_set("@a0", item)
   end
 
   def []=(index, item)
-    # old_count = instance_variables.count
-    instance_variables.reverse.each do |var|
-      num = var.to_s.scan(/\d/).join("").to_i
-      if num > index
-        instance_variable_set("@a#{num.to_i + 1}", instance_variable_get(var))
+    if index >= 0
+      diff = index - @size
+      if diff > 0
+        @size.upto(index).each { |num| instance_variable_set("@a#{num}", nil) }
+        @size += (diff + 1)
       end
+      instance_variable_set("@a#{index}", item)
+    else
+      raise IndexError if (index * -1) > @size
+      instance_variable_set("@a#{index % @size}", item)
     end
-    instance_variable_set("@a#{index}", item)
-
   end
 
   def <<(item)
